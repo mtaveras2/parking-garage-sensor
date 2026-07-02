c6fabc229bc6fe23f240483aef805375086f29a5
Maria <maria@dhcp-10-31-128-68.dyn.MIT.EDU>
2026-07-01 22:17:01 -0400

    Added clarifying comments

--- parking_garage_sensor.ino
+++ parking_garage_sensor.ino
@@ -1,5 +1,8 @@
-#include <Servo.h>
+// parking_garage_sensor.ino
+// Use Arduino IDE to run Code
 
+
+#include <Servo.h>
 
 #define trigPin 11
 #define echoPin 10
@@ -13,19 +16,20 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   digitalWrite(trigPin, LOW);
+
   parkingGate.attach(servoPin);
   parkingGate.write(0);
 }
 
 void loop() {
-  
+
   if (checkDistance() <= 5.00) {
     parkingGate.write(90);
     delay(5000);
-  
+
   } else {
     parkingGate.write(0);
-  
+
   }
 
   delay(100);
@@ -37,7 +41,7 @@ float checkDistance() {
   digitalWrite(trigPin, LOW);
 
   long duration = pulseIn(echoPin, HIGH);
-  float distance = (duration * .0343) / 2;    
+  float distance = (duration * .0343) / 2;
   Serial.println(distance);
   return distance;  // in cm
 }
